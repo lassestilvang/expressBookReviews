fa184f41ad2d546ff89db929f360d9138449003d
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-01-26 02:37:14 +0100

    Get book review by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -60,8 +60,12 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let book = books[req.params.isbn];
+  if (book) {
+    res.send(JSON.stringify(book.reviews, null, 4));
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
