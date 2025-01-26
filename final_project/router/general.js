8c2c8367bf65138127382fe08e0080f1b223c8c0
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-01-26 02:26:30 +0100

    Get book by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,13 @@ public_users.get("/", function (req, res) {
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  // Send JSON response based on the ISBN
+  let book = books[req.params.isbn];
+  if (book) {
+    res.send(JSON.stringify(book, null, 4));
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
 });
 
 // Get book details based on author
