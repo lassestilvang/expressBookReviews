93c80b349da4eeecdb312d17b1190bf83f7d83e3
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-01-26 03:23:08 +0100

    Use Promise callbacks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,52 +25,78 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-  // Send JSON response with formatted books data
-  res.send(JSON.stringify(books, null, 4));
+public_users.get("/", (req, res) => {
+  Promise.resolve()
+    .then(() => {
+      // Send JSON response with formatted books data
+      res.send(JSON.stringify(books, null, 4));
+    })
+    .catch((error) => {
+      console.error("Error fetching book list:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  // Send JSON response based on the ISBN
-  let book = books[req.params.isbn];
-  if (book) {
-    res.send(JSON.stringify(book, null, 4));
-  } else {
-    res.status(404).json({ message: "Book not found" });
-  }
+public_users.get("/isbn/:isbn", (req, res) => {
+  Promise.resolve()
+    .then(() => {
+      // Send JSON response based on the ISBN
+      let book = books[req.params.isbn];
+      if (book) {
+        res.send(JSON.stringify(book, null, 4));
+      } else {
+        res.status(404).json({ message: "Book not found" });
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching book:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
-  // 1. Obtain all the keys for the ‘books’ object.
-  // 2. Iterate through the ‘books’ array & check the author matches the one provided in the request parameters.
-  let authorBooks = [];
-  Object.keys(books).forEach((isbn) => {
-    if (books[isbn].author.toLowerCase() === req.params.author.toLowerCase()) {
-      authorBooks.push(books[isbn]);
-    }
-  });
-  if (authorBooks.length > 0) {
-    res.send(JSON.stringify(authorBooks, null, 4));
-  } else {
-    res.status(404).json({ message: "No books found by this author" });
-  }
+public_users.get("/author/:author", (req, res) => {
+  Promise.resolve()
+    .then(() => {
+      let authorBooks = Object.keys(books)
+        .filter(
+          (isbn) =>
+            books[isbn].author.toLowerCase() === req.params.author.toLowerCase()
+        )
+        .map((isbn) => books[isbn]);
+      if (authorBooks.length > 0) {
+        res.send(JSON.stringify(authorBooks, null, 4));
+      } else {
+        res.status(404).json({ message: "No books found by this author" });
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching books by author:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
-  let titleBooks = [];
-  Object.keys(books).forEach((isbn) => {
-    if (books[isbn].title.toLowerCase() === req.params.title.toLowerCase()) {
-      titleBooks.push(books[isbn]);
-    }
-  });
-  if (titleBooks.length > 0) {
-    res.send(JSON.stringify(titleBooks, null, 4));
-  } else {
-    res.status(404).json({ message: "No books found with this title" });
-  }
+public_users.get("/title/:title", (req, res) => {
+  Promise.resolve()
+    .then(() => {
+      let titleBooks = Object.keys(books)
+        .filter(
+          (isbn) =>
+            books[isbn].title.toLowerCase() === req.params.title.toLowerCase()
+        )
+        .map((isbn) => books[isbn]);
+      if (titleBooks.length > 0) {
+        res.send(JSON.stringify(titleBooks, null, 4));
+      } else {
+        res.status(404).json({ message: "No books found with this title" });
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching books by title:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
 //  Get book review
